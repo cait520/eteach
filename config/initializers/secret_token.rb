591bec490cf789e2f815f0bb038d4c8e2d60dc9e
cait520 <cait520@gmail.com>
2013-07-01 04:47:26 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eteach::Application.config.secret_token = 'a77c078490d27165cfcc6f87c743719303ba18a2d02772c1f1ff31e1213a74b012e7270452aad69062493f6517bb8faf9d04531722c8e3511aa38a33ba007933'
